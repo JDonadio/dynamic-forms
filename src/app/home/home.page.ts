d5654844e08ecf6d2c70c4c85dae1be258a08701
JDonadio <tanojaja@gmail.com>
2020-12-22 11:33:02 -0300

    dynamic input field

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  public dynamicForm: FormGroup;
+  public list = [];
 
+  constructor(
+    private fb: FormBuilder,
+  ) { }
+
+  ngOnInit() {
+    this.dynamicForm = this.createForm();
+  }
+
+  private createForm(): FormGroup {
+    this.list = this.getList();
+    let group: FormGroup | any = {};
+    let control: FormControl;
+
+    this.list.forEach(item => {
+      console.log(item)
+      control = this.fb.control('', item.required ? Validators.required : []);
+      group[item.id] = control;
+    })
+    return this.fb.group(group);
+  }
+
+  print() {
+    console.log('this.dynamicForm:', this.dynamicForm)
+  }
+
+  getList() {
+    return [
+      {
+        id: '1',
+        type: 'text',
+        label: 'User name',
+        required: true,
+      },
+      {
+        id: '2',
+        type: 'email',
+        label: 'Email',
+        required: false,
+      },
+      {
+        id: '3',
+        type: 'number',
+        label: 'DNI',
+        required: false,
+      },
+      {
+        id: '4',
+        type: 'phone',
+        label: 'Cellphone',
+        required: false,
+      },
+    ]
+  }
 }
